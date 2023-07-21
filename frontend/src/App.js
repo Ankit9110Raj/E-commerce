0b02a80f66a3a64b2574dfb6117de05181780a9a
Ankit Raj <rajankit17iitb@gmail.com>
2023-07-21 16:31:23 +0530

    Project initialized

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import NavbarTop from './components/Header/Navbar';
+
+function App() {
+  return (
+    <NavbarTop/>
+  );
+}
+
+export default App;
